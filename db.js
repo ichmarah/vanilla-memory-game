440fd416079fe0ad2dd4f1ef441860e8951aadd1
ichmarah <ichmarah@gmail.com>
2020-04-11 21:53:11 +0200

    Create local database

---
+++ db.js
@@ -0,0 +1,27 @@
+const cardArray = [
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png'
+  },
+  {
+    name: 'hotdog',
+    img: 'images/fries.png'
+  },
+  {
+    name: 'ice_cream',
+    img: 'images/ice-cream.png'
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png'
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png'
+  }
+];
+
